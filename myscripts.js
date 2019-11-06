441ca4834e21e43927ea7ba798a7857cc64986ad
codydegen <codydegen@gmail.com>
2019-11-06 17:18:54 -0500

    Add swapping functionality - nearly release ready

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,8 @@
 const POMODOROCOLOR = "rgb(0, 128, 0)";
 const BREAKCOLOR = "rgb(0, 0, 255)";
 const LONGBREAKCOLOR = "rgb(128, 0, 128)";
-
+//some weird skip issue
+// finalize colors
 document.getElementById("pomodoro").style.cssText = `background-color: ${POMODOROCOLOR}`;
 document.getElementById("break").style.cssText = `background-color: ${BREAKCOLOR}`;
 document.getElementById("longbreak").style.cssText = `background-color: ${LONGBREAKCOLOR}`;
@@ -25,18 +26,18 @@ resetAllButton.addEventListener('click', resetAll);
 const decrementButtons = document.querySelectorAll(".arrow")
 decrementButtons.forEach(button => button.addEventListener('click', lengthClick)); 
 const statusButtons = document.querySelectorAll(".status");
-statusButtons.forEach(button => button.addEventListener('click', function(e){
+/* statusButtons.forEach(button => button.addEventListener('click', function(e){
     const currentActive = document.getElementsByClassName("status active")[0];
     currentActive.classList.toggle("active");
     e.target.classList.add("active");
     toggleTime();
-}));
+})); */
 var timeId = true;
 
 function startTime(){
     // start the countdown
     if (timeId === true){
-        timeId = setInterval(countdown, 10);
+        timeId = setInterval(countdown, 1);
     }
 };
 
@@ -60,7 +61,7 @@ function visualUpdate(currentTiming, totalTiming) {
     const progressBar = document.getElementsByClassName("inProgress")[0];
     // console.dir(progressBar);
     let progressColor;
-    switch (progressBar.classList[0]) {
+    switch (progressBar.classList[1]) {
         case "pom":
             progressColor = POMODOROCOLOR;
             break;
@@ -86,6 +87,8 @@ function reset() {
     timeId = true;
     let newTime = parseInt(document.getElementsByClassName("time active")[0].textContent)*60;
     document.querySelector("#timer").textContent = secondToStringConversion(newTime);
+    let currentStatus = document.querySelector(".inProgress");
+    currentStatus.style.cssText = 'background-color: rgb(214, 214, 214)';
 
     // reset the clock to the selected time
 
@@ -96,11 +99,33 @@ function resetAll() {
     timeId = true;
     let newTime = parseInt(document.getElementsByClassName("time active")[0].textContent)*60;
     document.querySelector("#timer").textContent = secondToStringConversion(newTime);
-
+    let allStatus = document.querySelectorAll(".progress")
+    allStatus.forEach(item => item.style.cssText = 'background-color: rgb(214, 214, 214)');
+    let currentStatus = document.querySelector(".inProgress");
+    if(currentStatus !== null) currentStatus.classList.toggle("inProgress");
+    document.getElementById("visual1").classList.add("inProgress");
+    document.getElementById("label").innerText = "Pomodoro #1"
 };
 function skip() {
-    // still in progress
 
+    const visual = document.getElementsByClassName("inProgress")[0];
+    const visualNumber = visual.id[visual.id.length-1];
+    visual.classList.remove("inProgress");
+    visual.classList.add("complete");
+    let nextId;
+    let nextSession;
+    if (visualNumber === "8") {
+        nextId = "visual1";
+        nextSession = document.getElementById(nextId);
+
+    }else {
+        nextId = "visual"+parseInt(parseInt(visualNumber)+1);
+        nextSession = document.getElementById(nextId);
+    }
+
+    nextSession.classList.add("inProgress");
+    timeSwap();
+    reset();
 };
 
 function secondToStringConversion(seconds) {
@@ -115,6 +140,7 @@ function stringToSecondConversion(string) {
 };
 
 function lengthClick(e) {
+    
     let elementName = e.path[0].className.split(" ");
     let element;
     
@@ -136,6 +162,7 @@ function lengthClick(e) {
     }
     //console.dir(element);
     if (element.classList.contains("active")) {
+        reset();
         document.querySelector("#timer").textContent = element.innerText+":00";
     }
 
@@ -173,7 +200,7 @@ function timerEnd() {
     if (visualNumber === "8") {
         nextId = "visual1";
         nextSession = document.getElementById(nextId);
-
+        resetAll();
     }else {
         nextId = "visual"+parseInt(parseInt(visualNumber)+1);
         nextSession = document.getElementById(nextId);
@@ -187,5 +214,31 @@ function timerEnd() {
     //alert(`${session.id} is over.  Next up, ${nextSession}!`);
     clearInterval(timeId);
     timeId = true;
+    timeSwap();
     reset();
+};
+
+function timeSwap() {
+    const visual = document.getElementsByClassName("inProgress")[0];
+    const visualNumber = visual.id[visual.id.length-1];
+    const label = document.getElementById("label");
+    const currentActive = document.getElementsByClassName("status active")[0];
+    let newActive;
+    if (visualNumber%2 === 1) {
+        label.innerText = "Pomodoro #"+(parseInt(visualNumber)+1)/2;
+        currentActive.classList.toggle("active");
+        newActive = document.getElementById("pomodoro");
+        newActive.classList.add("active");
+    }else if (visualNumber == 8) {
+        label.innerText = "Long Break";
+        currentActive.classList.toggle("active");
+        newActive = document.getElementById("longbreak");
+        newActive.classList.add("active");
+    }else {
+        currentActive.classList.toggle("active");
+        newActive = document.getElementById("break");
+        newActive.classList.add("active");
+        label.innerText = "Break #"+parseInt(visualNumber)/2;
+    }
+    toggleTime();
 };
